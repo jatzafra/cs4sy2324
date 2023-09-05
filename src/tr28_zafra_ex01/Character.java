1056d0a451fd748c3a100af9a0ed953facb8d961
TRUTH <TRUTH@172.22.16.92>
2023-09-05 14:22:33 +0800

    Ex1

---
+++ src/tr28_zafra_ex01/Character.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr28_zafra_ex01;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class Character {
+    
+}
---
+++ src/tr28_zafra_ex01/Tr28_Zafra_Ex01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr28_zafra_ex01;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class Tr28_Zafra_Ex01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
