1349ff276089986d01e15afd59814d16eb36e952
TRUTH <>
2023-09-08 16:15:56 +0800

    Ex1

--- src/tr28_zafra_ex01/Character.java
+++ src/tr28_zafra_ex01/Character.java
@@ -10,5 +10,36 @@ package tr28_zafra_ex01;
  * @author TRUTH
  */
 public class Character {
+    String name;
+    int attackStat;
+    char tier;
+    int tierEquivalent;
     
+    Character(String name, int attackStat, char tier) {
+        this.name = name;
+        this.attackStat = attackStat;
+        this.tier = tier;
+        
+        switch(this.tier){
+            case 'S':
+                this.tierEquivalent = 6;
+                break;
+            case 'A':
+                this.tierEquivalent = 5;
+                break;
+            case 'B':
+                this.tierEquivalent = 4;
+                break;
+            case 'C':
+                this.tierEquivalent = 3;
+                break;
+            case 'D':
+                this.tierEquivalent = 2;
+                break;
+            case 'E':
+                this.tierEquivalent = 1;
+            case 'F':
+                this.tierEquivalent = 0;
+        }
+    }
 }
--- src/tr28_zafra_ex01/Tr28_Zafra_Ex01.java
+++ src/tr28_zafra_ex01/Tr28_Zafra_Ex01.java
@@ -15,7 +15,24 @@ public class Tr28_Zafra_Ex01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here\
+        Character ayaka = new Character("Ayaka", 342, 'S');
+        Character eula = new Character("Eula", 342, 'F');
+        Character ganyu = new Character("Ganyu", 335, 'B');
+        
+        System.out.println("Name: " + ayaka.name);
+        System.out.println("Attack Stat: " + ayaka.attackStat);
+        System.out.println("Tier: " + ayaka.tier + "\r\n");
+        System.out.println("Name: " + eula.name);
+        System.out.println("Attack Stat: " + eula.attackStat);
+        System.out.println("Tier: " + eula.tier + "\r\n");
+        System.out.println("Name: " + ganyu.name);
+        System.out.println("Attack Stat: " + ganyu.attackStat);
+        System.out.println("Tier: " + ganyu.tier + "\r\n");
+        
+        System.out.println("Total attack stat: " + (ayaka.attackStat + eula.attackStat + ganyu.attackStat));
+        System.out.println("Ayaka and Eula have the same attack stat: " + (ayaka.attackStat == eula.attackStat));
+        System.out.println("Eula has a higher tier than Ayaka: " + (eula.tierEquivalent > ayaka.tierEquivalent));
     }
     
 }
