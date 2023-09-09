39bc484fe4c831e6d711bd750abf66d00c6d06be
jatzafra <112681445+jatzafra@users.noreply.github.com>
2023-09-09 18:06:16 +0800

    Ex1

--- src/tr28_zafra_ex01/Character.java
+++ src/tr28_zafra_ex01/Character.java
@@ -13,10 +13,33 @@ public class Character {
     String name;
     int attackStat;
     char tier;
+    int tierEquivalent;
     
     Character(String name, int attackStat, char tier) {
         this.name = name;
         this.attackStat = attackStat;
         this.tier = tier;
+        
+        switch(this.tier){
+            case 'S':
+                this.tierEquivalent = 6;
+                break;
+            case 'A':
+                this.tierEquivalent = 5;
+                break;
+            case 'B':
+                this.tierEquivalent = 4;
+                break;
+            case 'C':
+                this.tierEquivalent = 3;
+                break;
+            case 'D':
+                this.tierEquivalent = 2;
+                break;
+            case 'E':
+                this.tierEquivalent = 1;
+            case 'F':
+                this.tierEquivalent = 0;
+        }
     }
 }
