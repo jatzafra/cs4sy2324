7d9feb09feb027bb460a5eecaff16708032ffcda
TRUTH <TRUTH@172.22.16.92>
2023-09-12 14:26:55 +0800

    Ex2

---
+++ src/tr28_zafra_ex02/Singer.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tr28_zafra_ex02;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class Singer {
+    String name;
+    int noOfPerformances;
+    double earnings;
+    Song favoriteSong;
+    
+    Singer(String name, int perform, double earn, Song favorite){
+        this.name = name;
+        this.noOfPerformances = perform;
+        this.earnings = earn;
+        this.favoriteSong = favorite;
+    }
+    public void performForAudience(int n){
+        noOfPerformances++;
+        earnings += n*100;
+    }
+    public void changeFavSong(Song favorite){
+        favoriteSong = favorite;
+    }
+}
---
+++ src/tr28_zafra_ex02/Tr28_Zafra_Ex02.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tr28_zafra_ex02;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class Tr28_Zafra_Ex02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        Song moonlikeSmile = new Song("Moonlike Smile");
+        Song inevitableConflict = new Song("Inevitable Conflict");
+        
+        Singer yuPengChen = new Singer("Yu-Peng Chen", 0, 0, moonlikeSmile);
+        
+        yuPengChen.performForAudience(12);
+        System.out.println("Name: " + yuPengChen.name);
+        System.out.println("No. of Performances: " + yuPengChen.noOfPerformances);
+        System.out.println("Earnings: " + yuPengChen.earnings);
+        System.out.println("Favorite Song: " + yuPengChen.favoriteSong + "\r\n");
+        
+        yuPengChen.changeFavSong(inevitableConflict);
+        System.out.println("Name: " + yuPengChen.name);
+        System.out.println("No. of Performances: " + yuPengChen.noOfPerformances);
+        System.out.println("Earnings: " + yuPengChen.earnings);
+        System.out.println("Favorite Song: " + yuPengChen.favoriteSong);
+        
+        
+    }
+    
+}
