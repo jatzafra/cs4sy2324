04d1e1ced6a977654f9e0dab3c704151c4ec60e5
TRUTH <TRUTH@172.22.16.92>
2023-11-07 14:07:17 +0800

    lecture on inheritance&polymorphism

---
+++ src/lecture_inheritancePolymorphism/Student.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lecture_inheritancePolymorphism;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class Student extends Person{
+    private double grade;
+    private int level;
+    private String section;
+    
+    public Student(String name, double grade, int level, String section){
+        super(name);
+        this.grade = grade;
+        this.level = level;
+        this.section = section;
+    }
+
+    /**
+     * @return the grade
+     */
+    public double getGrade() {
+        return grade;
+    }
+
+    /**
+     * @param grade the grade to set
+     */
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    /**
+     * @return the level
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * @param level the level to set
+     */
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    /**
+     * @return the section
+     */
+    public String getSection() {
+        return section;
+    }
+
+    /**
+     * @param section the section to set
+     */
+    public void setSection(String section) {
+        this.section = section;
+    }
+}
