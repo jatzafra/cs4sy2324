8e3335b5cd17530da62d99fe69a79a3c32f07517
TRUTH <TRUTH@172.22.16.92>
2023-11-14 13:48:48 +0800

    ex6 rename

---
+++ src/exercise06truthzafrajulian/FireType.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise06truthzafrajulian;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class FireType extends Monster{
+    public FireType(String n, int m, int base){
+        super(n, "fire", "grass", "water", m, base);
+        atk *= 1.3;
+        def *= 0.7;
+    }
+    @Override
+    public void special(){
+        System.out.println(name + " used its special attack.");
+        atk += 2;
+        hp -= 0.1*maxHP;
+    }
+}
---
+++ src/exercise06truthzafrajulian/WaterType.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise06truthzafrajulian;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class WaterType extends Monster{
+    public WaterType(String n, int m, int base){
+        super(n, "water", "fire", "grass", m, base);
+        atk *= 0.7;
+        def *= 1.3;
+    }
+    @Override
+    public void special(){
+        System.out.println(name + " used its special attack.");
+        def += 2;
+        hp -= 0.1*maxHP;
+    }
+}
